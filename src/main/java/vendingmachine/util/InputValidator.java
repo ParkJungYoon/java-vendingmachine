9953839da84b8b668deffb91557a257be5b9ed83
parkjungyoon <tbr06057@naver.com>
2022-12-11 17:45:58 +0900

    feat: 상품 가격이 100보다 작은 경우 예외처리

--- src/main/java/vendingmachine/util/InputValidator.java
+++ src/main/java/vendingmachine/util/InputValidator.java
@@ -11,9 +11,11 @@ public class InputValidator {
     private final static int UNIT = 10;
     private final static int INPUT_SIZE = 3;
     private final static int MIN_RANGE_NUM = 10;
+    private final static int MIN_PRICE = 100;
     private final static String INVALID_UNIT_ERROR_MESSAGE = "[ERROR] 10으로 나누어 떨어지는 숫자여야 합니다.";
     private final static String NON_NUMERIC_ERROR_MESSAGE = "[ERROR] 숫자로만 이루어진 값을 입력해주세요.";
     private final static String AMOUNT_RANGE_ERROR_MESSAGE = "[ERROR] 10이상 숫자를 입력해주세요.";
+    private final static String PRICE_RANGE_ERROR_MESSAGE = "[ERROR] 100이상 숫자를 입력해주세요.";
     private final static String INVALID_FORMAT_ERROR_MESSAGE = "[ERROR] 올바른 형식의 값이 아닙니다.";
     private final static String NON_EXIST_PRODUCT_ERROR_MESSAGE = "[ERROR] 존재하지 않는 상품입니다.";
     private final static String OUT_OF_SIZE_ERROR_MESSAGE = "[ERROR] 상품명, 가격, 수량 총 3가지를 입력해주세요.";
@@ -38,6 +40,12 @@ public class InputValidator {
         }
     }
 
+    public static void validatePriceRange(int input) {
+        if (input < MIN_PRICE) {
+            throw new IllegalArgumentException(PRICE_RANGE_ERROR_MESSAGE);
+        }
+    }
+
     public static void validateProduct(String input) {
         String[] products = input.split(";");
         for (String product : products) {
@@ -46,6 +54,7 @@ public class InputValidator {
             validateSize(transformProduct);
             validateNonNumeric(transformProduct.get(1));
             validateNonNumeric(transformProduct.get(2));
+            validatePriceRange(Integer.parseInt(transformProduct.get(1)));
         }
     }
 
