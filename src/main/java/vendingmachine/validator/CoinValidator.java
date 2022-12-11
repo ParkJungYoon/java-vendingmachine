f8ad37e42956f27f983cce6919078d2ed20e2813
parkjungyoon <tbr06057@naver.com>
2022-12-11 14:15:00 +0900

    feat: 자판기가 보유하고 있는 금액에 대한 예외처리

---
+++ src/main/java/vendingmachine/validator/CoinValidator.java
@@ -0,0 +1,29 @@
+package vendingmachine.validator;
+
+public class CoinValidator {
+    private final static int UNIT = 10;
+    private final static int MIN_RANGE_NUM = 10;
+    private final static String INVALID_UNIT_ERROR_MESSAGE = "[ERROR] 10으로 나누어 떨어지는 숫자여야 합니다.";
+    private final static String NON_NUMERIC_ERROR_MESSAGE = "[ERROR] 숫자로만 이루어진 값을 입력해주세요.";
+    private final static String AMOUNT_RANGE_ERROR_MESSAGE = "[ERROR] 10이상 숫자를 입력해주세요.";
+
+    public static void validateNonNumeric(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NON_NUMERIC_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateUnit(int input) {
+        if (input % UNIT != 0) {
+            throw new IllegalArgumentException(INVALID_UNIT_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateAmountRange(int input) {
+        if (input < MIN_RANGE_NUM) {
+            throw new IllegalArgumentException(AMOUNT_RANGE_ERROR_MESSAGE);
+        }
+    }
+}
